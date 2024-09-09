d22c5c4ed74528602c73f21daa5efdfd7b4f4c4a
stegmaja <58080521+stegmaja@users.noreply.github.com>
2024-09-09 14:02:37 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mkdir plots
 mkdir data
-rm -rf mobse
+pip install -r requirements.txt
 git clone git@gitlab.com:mobse/source-code.git mobse
 cd mobse
 patch -p1 < ../mobse.patch
 cd src
 make mobse
 cd ../..
-pip install -r requirements.txt
